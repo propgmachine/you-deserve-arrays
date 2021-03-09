11a9b392c0e15f7d501c5457a6a0282c396c4ce3
propgmachine <77746698+propgmachine@users.noreply.github.com>
2021-03-09 15:09:43 -0500

    updated Main.js

--- main.js
+++ main.js
@@ -2,13 +2,40 @@
  * YOUR CODE HERE *
  ******************/
 
+function getFirstItemFrom(arr) {
+  return arr[0];
+}
+
+function getLastItemFrom(arr) {
+  return arr[arr.length - 1];
+}
+
+function getIndex3(arr) {
+  if (arr.length < 4) {
+    return arr.[arr.length - 1];
+  } else {
+    return arr[3];
+  }
+}
+
+function isLongList(arr) {
+  return arr.length >= 10;
+}
+
+function firstItemIsNumber(arr) {
+  const firstItem = arr[0];
+  return typeof firstItem === 'number';
+}
+
+function secondCharOfThirdString(arr) {
+  const thirdItem = arr[2];
+  return thirdItem[1];
+}
 
 
 
 
-
-
- /********************************************
+/********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
  *           PLEASE DON'T TOUCH!            *
  ********************************************/
